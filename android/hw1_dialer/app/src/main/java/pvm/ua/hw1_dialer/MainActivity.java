ac5739a93f1ca5634a1e09011634c28dd416125e
Pedro Valente <pmateus1999@gmail.com>
2021-12-05 16:59:13 +0000

    all logic done, missing ui

--- android/hw1_dialer/app/src/main/java/pvm/ua/hw1_dialer/MainActivity.java
+++ android/hw1_dialer/app/src/main/java/pvm/ua/hw1_dialer/MainActivity.java
@@ -2,14 +2,19 @@ package pvm.ua.hw1_dialer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    AlertDialog.Builder dialogBuilder;
+    AlertDialog dialog;
 
     Button btnOne;
     Button btnTwo;
@@ -27,6 +32,18 @@ public class MainActivity extends AppCompatActivity {
     EditText numberEt;
     Button btnDial;
     Button btnDelete;
+
+    Button btnMem1;
+    Button btnMem2;
+    Button btnMem3;
+
+    String mem1;
+    String mem2;
+    String mem3;
+
+    EditText mem_name;
+    Button popup_saveBtn;
+    Button getPopup_cancelBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +65,9 @@ public class MainActivity extends AppCompatActivity {
         btnZero = findViewById(R.id.buttonZero);
         btnHashtag = findViewById(R.id.buttonHastag);
         btnDelete = findViewById(R.id.buttonDelete);
+        btnMem1 = findViewById(R.id.memory1);
+        btnMem2 = findViewById(R.id.memory2);
+        btnMem3 = findViewById(R.id.memory3);
 
         btnOne.setOnClickListener(v -> onClickButton(btnOne, numberEt, "1"));
 
@@ -91,6 +111,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnMem1.setOnLongClickListener(v -> {
+            mem1 = numberEt.getText().toString();
+            createDialog(btnMem1);
+            numberEt.setText("");
+            return false;
+        });
+
+
+
+        btnMem1.setOnClickListener(v -> numberEt.setText(mem1));
+
+        btnMem2.setOnLongClickListener(v -> {
+            mem2 = numberEt.getText().toString();
+            createDialog(btnMem2);
+            numberEt.setText("");
+            return false;
+        });
+
+        btnMem2.setOnClickListener(v -> numberEt.setText(mem2));
+
+        btnMem3.setOnLongClickListener(v -> {
+            mem3 = numberEt.getText().toString();
+            createDialog(btnMem3);
+            numberEt.setText("");
+            return false;
+        });
+
+        btnMem3.setOnClickListener(v -> numberEt.setText(mem3));
+
+
         numberEt = findViewById(R.id.numberEt);
 
     }
@@ -100,5 +150,34 @@ public class MainActivity extends AppCompatActivity {
         inputNumber.setText(cache + number);
     }
 
+
+    public void createDialog(Button btn){
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View memNameView = getLayoutInflater().inflate(R.layout.popup, null);
+        mem_name = memNameView.findViewById(R.id.name);
+
+        popup_saveBtn = memNameView.findViewById(R.id.saveButton);
+        getPopup_cancelBtn = memNameView.findViewById(R.id.cancelButton);
+
+        dialogBuilder.setView(memNameView);
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+        popup_saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn.setText(mem_name.getText());
+                dialog.dismiss();
+            }
+        });
+
+        getPopup_cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+    }
 
 }
